4eff3c21f46c61940116f42522dfc69c405d02c8
Your Name <kulikova.viktori@mail.ru>
2020-03-25 10:16:02 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Page header
+# An Introduction to Bootstrap 4
+How to use this?
+Add to the head of your page next strings:
+
+```
+  <link rel="stylesheet" href="dist/bootstrap.css"> <!-- Customized bootstrap -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900&display=swap&subset=latin-ext"
+    rel="stylesheet"> <!-- google cdn base font -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap&subset=cyrillic"
+    rel="stylesheet"> <!-- google cdn cyrillic base font -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"> <!-- font awesome (used on menu icons) -->
+```
+
+# Why I can't build this project? 
+KendoUI from the Node Modules is not build correctly out the box; At first you are need to correct an Errors of compliler:
+_variables.scss
+$calendar-view-height: $calendar-cell-size * 8;// calc( #{$calendar-cell-size * 8} + #{$calendar-header-height} ) !default;
+$calendar-cell-size: ($calendar-cell-size-px / $font-size) * 1em !default;
+
+================
+_layout.scss
+.k-calendar-view {
+	width: $calendar-cell-size;// ;(7 * $calendar-cell-size);
+	height: $calendar-cell-size;// (7 * $calendar-cell-size);
+}
+
+
+# How to start?
+1. `npm run build` to build styles
+2. `npm run dev` to watch files
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,113 @@
-const { dest, src, series, parallel } = require('gulp')
-const del = require('del')
-const browserSync = require('browser-sync')
-const watch = require('gulp-watch')
+const { dest, src, series, parallel } = require('gulp');
+const del = require('del');
+const rigger = require('gulp-rigger');
+const browserSync = require('browser-sync');
+const watch = require('gulp-watch'); 
+const replace = require('gulp-string-replace');
+const sass = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const prefix = require("gulp-autoprefixer");
+const sourceMap = require("gulp-sourcemaps");
+const googleWebFonts = require("gulp-google-webfonts");
+const cssnano = require('gulp-cssnano');
+const gulpCopy = require('gulp-copy'); 
+const debug = require('gulp-debug');
 
 // file locations
-const buildFolder = './build'
-const srcFolder = './src'
-const indexFile = `${srcFolder}/index.html`
-const cssFiles = `${srcFolder}/styles/*.css`
-const assestFiles = [
-  `${srcFolder}/assets/*.ico`,
-  `${srcFolder}/assets/*.svg`,
-  `${srcFolder}/assets/*.jpg`,
-  `${srcFolder}/assets/*.png`
-]
+const buildFolder = './build';
+const srcFolder = './src';
+const options = {};
+const indexFile = [
+  `${srcFolder}/src/*.html`, 
+  `${srcFolder}/src/components/*.html`, 
+  `${srcFolder}/src/components/**/*.html`, 
+  `${srcFolder}/src/pages/*.html`
+];
+
+const cssFiles = `${srcFolder}/styles/bootstrap.scss`;
+
+const assestFiles = `${srcFolder}/assets/**/*`;
+
+const handleError = err => {
+  console.log(err.toString());
+  this.emit('end');
+};
 
 // build files
-const css = () => src(cssFiles).pipe(dest(buildFolder))
-const html = () => src(indexFile).pipe(dest(buildFolder))
-const assets = () => src(assestFiles).pipe(dest(buildFolder))
+
+const css = () => {
+  console.log("Build minified bootstrap");
+  return src(cssFiles)
+      .pipe(sourceMap.init())
+      .pipe(sass())
+      .pipe(replace('/assets/img', '/assets/bootstrap-compiled/assets/img'))
+      .pipe(prefix({
+          cascade: false
+      }))
+      .pipe(cleanCSS({
+          compatibility: "ie8"
+      }))
+      .pipe(rename("bootstrap.min.css"))
+      .pipe(cssnano())
+      .pipe(sourceMap.write("./"))
+      .pipe(dest(`${buildFolder}/src/style`));
+};
+
+const html = () => {
+  console.log("Building HTML in progress...");
+
+  return src(indexFile, {base:'src'})
+        .pipe(rigger()).on('error', handleError)
+        .pipe(dest(buildFolder));
+};
+
+const js = () => {
+  console.log("Copy bootstrap javascript dependencies");
+  return src([
+          'node_modules/bootstrap/dist/js/bootstrap.js',
+          'node_modules/jquery/dist/jquery.js',
+          'node_modules/popper.js/dist/umd/popper.js'
+        ])
+        .pipe(dest(`${buildFolder}/src/js`));
+};
+
+const fonts = () => {
+  console.log("Fetching woff fonts from google");
+  return src(`${srcFolder}/assets/fonts/fonts.list`)
+        .pipe(googleWebFonts(options))
+        .pipe(debug())
+        .pipe(dest(`${buildFolder}/src/fonts`));
+};
+
+const assets = () => {
+  console.log("Copying assets from ./assets to dist");
+  return src(assestFiles)
+        .pipe(gulpCopy(buildFolder));
+};
+
+const copy_img = () => {
+  console.log("Copy img");
+  return src([
+            'node_modules/flag-icon-css/flags/4x3/*'
+        ])
+        .pipe(dest(`${buildFolder}/src/assets/img/flags/4x3/`));
+};
 
 //watch files
-const watchCss = () => watch(cssFiles, css)
-const watchHtml = () => watch(indexFile, html)
+const watchCss = () => watch(`${srcFolder}/styles/**/*.{scss,sass}`, css);
+const watchHtml = () => watch(indexFile, html);
 
 // utils
-const clean = () => del(buildFolder)
+const clean = () => del(buildFolder);
+
 const devServer = () =>
   browserSync.init({
-    server: buildFolder,
+    server: `${buildFolder}/src`,
     files: [`${buildFolder}/**`]
   })
 
-const build = series(clean, parallel(html, css, assets))
+const build = series(clean, parallel(html, css, js, assets, copy_img))
 const watchFiles = parallel(watchCss, watchHtml)
 
 exports.default = build
